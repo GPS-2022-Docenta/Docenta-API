eef209e1016094c00563f31f945ae662ddc73816
JorgeBernalRomero <JorgeBernalRomero@users.noreply.github.com>
2023-01-04 18:37:21 +0100

    Borrado un comentario para el cors.

--- index.js
+++ index.js
@@ -13,25 +13,6 @@ const PORT = process.env.PORT || 3050;
 
 const app = express();
 
-/* var allowedOrigins = ["https://docenta.vercel.app"];
-
-var corsOptions = {
-  origin: function (origin, callback) {
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-};
-
-app.use(cors(corsOptions)); */
-
 app.use(cors());
 app.use(bodyParser.json());
 
